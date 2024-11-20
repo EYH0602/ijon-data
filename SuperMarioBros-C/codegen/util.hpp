cb7fc03e388a308010b179c065c4085e35972d2a
Yifeng He <yfhe.prsn@gmail.com>
2024-11-19 17:35:32 -0800

    format SMB

--- SuperMarioBros-C/codegen/util.hpp
+++ SuperMarioBros-C/codegen/util.hpp
@@ -3,18 +3,17 @@
 
 #include <string>
 
-void mapComment(int line, const std::string& comment);
+void mapComment(int line, const std::string &comment);
 void mapNewline(int line);
-const char* lookupComment(int line);
+const char *lookupComment(int line);
 
 // We need to be able to duplicate yytext since it is volatile
 //
-inline char* duplicateString(const char* s, size_t n)
-{
-    char* d = new char[n + 1];
-    memcpy(d, s, n);
-    d[n] = '\0';
-    return d;
+inline char *duplicateString(const char *s, size_t n) {
+  char *d = new char[n + 1];
+  memcpy(d, s, n);
+  d[n] = '\0';
+  return d;
 }
 #define DUP_YYTEXT (duplicateString(yytext, yyleng))
 
